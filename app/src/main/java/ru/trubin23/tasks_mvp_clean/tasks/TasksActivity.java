f113a8098786ef2ec379a3d295b13b6bf1e6e973
Trubin Andrey <trubin23@gmail.com>
2019-01-13 11:12:02 +0500

    fill tasks_act layout

--- app/src/main/java/ru/trubin23/tasks_mvp_clean/tasks/TasksActivity.java
+++ app/src/main/java/ru/trubin23/tasks_mvp_clean/tasks/TasksActivity.java
@@ -11,5 +11,7 @@ public class TasksActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tasks_act);
+
+
     }
 }
